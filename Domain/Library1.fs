5d8a50a302036bea4fea120b9facc47db79baad2
Hackle Wayne <hacklew@hotmail.com>
2016-10-24 21:40:22 +1300

    Can fold

---
+++ Domain/Library1.fs
@@ -0,0 +1,18 @@
+﻿namespace Domain
+
+module Model =
+    type Dependency<'a> = { From: 'a; To: 'a list }
+
+    let print all = 
+        let rec sprintOne ltr =
+            let sprintChildren = sprintfn "parent %s has children %s" ltr
+
+    [  
+        { From = 'B'; To = [ 'C'; 'E' ] };
+        { From = 'A'; To = [ 'B'; 'C' ] };
+        { From = 'C'; To = [ 'G' ] };
+        { From = 'D'; To = [ 'A'; 'F' ] };
+        { From = 'E'; To = [ 'F' ] };  
+        { From = 'F'; To = [ 'H' ] };
+    ]
+    |> calculate
