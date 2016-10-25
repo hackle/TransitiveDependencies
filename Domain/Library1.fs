85b35345e5d21cf2b21a6c68876587247d08307b
Hackle Wayne <hacklew@hotmail.com>
2016-10-25 21:43:33 +1300

    Have good fold back!

--- Domain/Library1.fs
+++ Domain/Library1.fs
@@ -3,9 +3,31 @@
 module Model =
     type Dependency<'a> = { From: 'a; To: 'a list }
 
-    let print all = 
-        let rec sprintOne ltr =
-            let sprintChildren = sprintfn "parent %s has children %s" ltr
+    let findDependencies all = 
+        let rec findForOne carry key =
+            let found = all |> List.tryFind (fun d -> d.From = key)
+            match found with
+            | None -> carry
+            | Some f -> f.To |> List.fold findForOne (f.To @ carry)
+
+        all |> List.map (fun d -> { From = d.From; To = d.From |> findForOne [] })
+
+    let findWithFoldBack all =
+        let rec findForOne (key: 'a) (cont: 'a list -> 'a list) =
+            let found = all |> List.tryFind (fun d -> d.From = key)
+            match found with
+            | None -> cont []
+            | Some f ->             
+                let folder racc cur =
+                    findForOne cur (fun rcur -> rcur @ racc)
+                f.To |> List.fold folder f.To
+
+        all |> List.map (fun d -> { 
+                                    From = d.From; 
+                                    To = findForOne d.From id 
+                                            |> List.distinct 
+                                            |> List.sort
+                                    })
 
     [  
         { From = 'B'; To = [ 'C'; 'E' ] };
@@ -15,4 +37,4 @@ module Model =
         { From = 'E'; To = [ 'F' ] };  
         { From = 'F'; To = [ 'H' ] };
     ]
-    |> calculate
+    |> findWithFoldBack
